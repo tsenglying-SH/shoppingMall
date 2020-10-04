a83d317dfa0900c4fddfd1f11e51f41b9fc8b0ca
unknown <you@example.com>
2020-10-04 14:53:22 +0800

    parent project

---
+++ src/test/java/com/shoppingmall/parent/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.parent.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
