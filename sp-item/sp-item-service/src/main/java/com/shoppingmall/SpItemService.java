ece9e9220e660d0c99e25f24e0571fa1893329e7
unknown <you@example.com>
2020-10-04 20:52:05 +0800

    item common and utils

---
+++ sp-item/sp-item-service/src/main/java/com/shoppingmall/SpItemService.java
@@ -0,0 +1,20 @@
+package com.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName SpItemService
+ * @Description TODO
+ * @Author Tsenglying
+ * @Date 2020/10/4 16:27
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient/*注册到eureka中*/
+public class SpItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(SpItemService.class);
+    }
+}
